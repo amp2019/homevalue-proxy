f0e0cc2bc6f49b2fa2c1df3eb7592d2c0daabd27
wizard-jS <spraguej624@gmail.com>
2019-02-24 13:04:09 -0800

    Clean code

--- server/index.js
+++ server/index.js
@@ -4,22 +4,19 @@ const path = require('path');
 
 const app = express();
 const port = 8081; 
-// const serve = express.static('../client/');
-// app.get('/', function(req, res){
-//   res.send('hello world');
-// });
 
 app.use('/', express.static(path.join(__dirname, '../client')));
-
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(bodyParser.json());
+
 app.get('/:houseId', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 });
