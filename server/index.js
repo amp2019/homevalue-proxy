15110acf33c4bf20894bdc7bdd4d5deea1721945
Mandy Trinh <saphiric@gmail.com>
2019-03-08 16:11:46 -0800

    Add redirect for all CRUD endpoints

--- server/index.js
+++ server/index.js
@@ -13,8 +13,29 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json());
 
-app.get('/:houseId', (req, res) => {
+app.get('/:propertyId', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+
+app.get('/api/properties/:propertyId', (request, response) => {
+  console.log('HIT GET REDIRECT');
+  response.redirect('http://localhost:8081/api/properties/:propertyId');
+});
+
+app.post('/post', (request, response) => {
+  console.log('HIT POST REDIRECT, body IS ', request.body);
+  response.redirect(307, 'http://localhost:8081/post');
+});
+
+app.delete('/delete/:propertyId', (request, response) => {
+  let id = request.params.propertyId;
+  console.log('HIT DELETE REDIRECT', id );
+  response.redirect(303, 'http://localhost:8081/delete/:propertyId');
+});
+
+app.put('/update', (request, response) => {
+  console.log('HIT UPDATE REDIRECT, body IS ', request.body);
+  response.redirect(307, 'http://localhost:8081/update');
 });
 
 app.listen(port, () => {
